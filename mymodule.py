e3266aabc29e9e9833ba205d232ed12c181c276f
Alex Berezovsky <oxonkoalexey@gmail.com>
2025-05-31 09:11:28 +0300

    Working with a Python module

---
+++ mymodule.py
@@ -0,0 +1,15 @@
+def greeting(name):
+  print("Hello, " + name)
+
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
+person = {
+  "name": "Bob",
+  "age": 25,
+  "country": "USA"
+}
+
+
